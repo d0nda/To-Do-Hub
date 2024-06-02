0a1e302b542d4d75cc6f24f498445699aad81141
Nkosinathi Mbokazi <dondahq@gmail.com>
2024-06-02 19:16:38 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-//App.js
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { enableScreens } from 'react-native-screens';
+import { StatusBar } from 'react-native';
 import TodoScreen from './src/screens/TodoScreen';
 import AddToDoScreen from './src/screens/AddToDoScreen';
 
+enableScreens();
+
 const Stack = createStackNavigator();
 
-const App = () => {
+export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="light" backgroundColor="#6a51ae" translucent={false} />
       <Stack.Navigator>
-        <Stack.Screen name="TodoScreen" component={TodoScreen} options={{ title: 'To-Do Hub' }} />
-        <Stack.Screen name="AddToDoScreen" component={AddToDoScreen} options={{ title: 'Add To Do' }} />
+        <Stack.Screen 
+          name="TodoScreen" 
+          component={TodoScreen} 
+          options={{ title: 'To-Do Hub' }} 
+        />
+        <Stack.Screen 
+          name="AddToDoScreen" 
+          component={AddToDoScreen} 
+          options={{ title: 'Add To Do' }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
--- src/screens/AddToDoScreen.js
+++ src/screens/AddToDoScreen.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, KeyboardAvoidingView, Platform, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { SafeAreaView, KeyboardAvoidingView, Platform, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
 import { RichText, Toolbar, useEditorBridge } from '@10play/tentap-editor';
 
 const AddToDoScreen = ({ route }) => {
@@ -9,22 +9,22 @@ const AddToDoScreen = ({ route }) => {
   const { todo } = route.params || {};
   const navigation = useNavigation();
 
+  // Initialize the 10tap editor
   const editor = useEditorBridge({
     autofocus: true,
     avoidIosKeyboard: true,
     initialContent: todo ? todo.text : '',
   });
 
- 
- 
   useEffect(() => {
     if (todo) {
-      //console.log("Todo Text:", todo.text);
+      // Set initial content of the editor to the text of the todo
+      console.log("Todo Text:", todo.text);
       editor.setContent(todo.text);
     }
-  }, [todo]);
+  }, [todo, editor]);
 
-  const addTodo = () => {
+  const addTodo = useCallback(() => {
     editor.getText().then((content) => {
       if (content.trim() !== '') {
         if (todo) {
@@ -32,25 +32,34 @@ const AddToDoScreen = ({ route }) => {
           navigation.navigate('TodoScreen', { newTodo: updatedTodo });
         } else {
           const newTodo = { id: Date.now().toString(), text: content, createdAt: new Date().toISOString() };
-          navigation.navigate('TodoScreen', { newTodo}); 
+          navigation.navigate('TodoScreen', { newTodo }); 
         }
         setText('');
       }
     });
-  };
+  }, [editor, navigation, todo]);
+
+  useFocusEffect(
+    useCallback(() => {
+      navigation.setOptions({
+        headerRight: () => (
+          <TouchableOpacity style={styles.addButton} onPress={addTodo}>
+            <Icon name="plus" size={20} color="white" />
+          </TouchableOpacity>
+        ),
+      });
+    }, [navigation, addTodo])
+  );
 
   return (
     <SafeAreaView style={styles.fullScreen}>
       <RichText editor={editor} />
       <KeyboardAvoidingView
-        behavior={Platform.OS === 'android' ? 'padding' : 'height'}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.keyboardAvoidingView}
       >
-      <Toolbar editor={editor} />
+        <Toolbar editor={editor} />
       </KeyboardAvoidingView>
-      <TouchableOpacity style={styles.addButton} onPress={addTodo}>
-          <Icon name="plus" size={20} color="white" />
-        </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -65,9 +74,7 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
   addButton: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
+    marginRight: 10,
     backgroundColor: 'blue',
     width: 40,
     height: 40,
